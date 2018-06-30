dc5da6136bfdcf4135ad527ddc4be1d6e36cb828
stacy-eliz <anastasiya.elizarowa2017@yandex.ru>
2018-06-30 22:51:49 +0400

    test cancel the move

--- handler.py
+++ handler.py
@@ -175,7 +175,8 @@ def handle_dialog(request, response, user_storage):
                     user_storage["alices_matrix"] = user_storage["last_turn_field"][0]
                     user_storage["users_matrix"] = user_storage["last_turn_field"][1]
                     response.set_text('Предыдущий ваш ход и ход Алисы отменены.')
-                except:
+                except Exception as e:
+                    print(e)
                     response.set_text('Невозможно отменить ход')
 
             # Проверка наличия слова в словах о начале игры
